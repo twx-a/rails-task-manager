69f963cd4c740af144476a6b580eee70c5f8e01a
Alfred Tay <twx.alfred@gmail.com>
2022-08-17 12:05:59 +0800

    added show method for individual links for instances ids

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,16 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def add
+    @tasks = Task.new
+  end
+
+  def create
+    @tasks = Task.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/create', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
