4b59ca6286712d08aa56d142989a216843a344c0
Alfred Tay <twx.alfred@gmail.com>
2022-08-17 12:50:26 +0800

    done with all just go thru code and read what ive done. TODO: ADD STYLE

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -17,9 +17,26 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+  end
+
+  def update
+    @task.update(form_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
 
   def form_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # home page
   get 'tasks', to: 'tasks#index'
+  # create new task
   get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
+  # show single task
   get 'tasks/:id', to: 'tasks#show', as: :task
+  # update task
+  get 'task/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  # delete task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
